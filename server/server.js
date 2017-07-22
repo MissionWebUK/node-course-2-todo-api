781d94f5766a28dd0700712e454318e47ee69941
Martin Sanders <martin26sanders@btinternet.com>
2017-07-22 11:45:14 +0100

    Setup App for deployment to VPS

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ var {User} = require('./models/user');
 
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -81,9 +83,9 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
 
-  console.log('started on port 3000');
+  console.log(`Server started on port ${port}`);
 
 });
 
