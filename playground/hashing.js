20c6ddbc3a47d867a8214fd693d018420415e3e0
Martin Sanders <martin26sanders@btinternet.com>
2017-07-23 19:15:52 +0100

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -2,19 +2,41 @@ const {SHA256} = require('crypto-js');
 
 const jwt = require('jsonwebtoken');
 
-var data = {
+const bcrypt = require('bcryptjs');
 
-  id: 10
+var password = '123abc!';
 
-};
+// bcrypt.genSalt(10, (err, salt) => {
+//
+//   bcrypt.hash(password, salt, (err, hash) => {
+//
+//     console.log(hash);
+//
+//   });
+//
+// });
 
-var token = jwt.sign(data, '123abc');
+var hashedPassword = '$2a$10$iYlub84Zqkw0jCQI9froVO98x2BSVUXQJvun6b/zYu5ersl6L2zEq';
 
-console.log(token);
+bcrypt.compare(password, hashedPassword, (err, res) => {
 
-var decoded = jwt.verify(token, '123abc');
+  console.log(res);
 
-console.log('decoded', decoded);
+});
+
+// var data = {
+//
+//   id: 10
+//
+// };
+//
+// var token = jwt.sign(data, '123abc');
+//
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+//
+// console.log('decoded', decoded);
 
 // var message = 'I am user number 3';
 //
