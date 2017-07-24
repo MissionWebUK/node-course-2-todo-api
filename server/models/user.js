612e8962e9d1daf80b5046df4e6102612530c41f
Martin Sanders <martin26sanders@btinternet.com>
2017-07-24 21:13:08 +0100

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -81,6 +81,23 @@ UserSchema.methods.generateAuthToken = function () {
 
 };
 
+
+UserSchema.methods.removeToken = function (token) {
+
+  var user = this;
+
+  return user.update({
+
+    $pull: {
+
+      tokens: {token}
+
+    }
+
+  })
+
+};
+
 UserSchema.statics.findByToken = function (token) {
 
   var User = this;
